a4aebe0816eedd81275f29acdf72250f484b44d9
papistan <jpapisan@gmail.com>
2018-07-23 20:07:13 -0700

    Adds functions for student data fetching, student ranking, and appending student rows to table

--- src/static/script.js
+++ src/static/script.js
@@ -2,6 +2,15 @@ const AllExamsTable = document.getElementById("all-exams-table");
 const SingleExamTable = document.getElementById("single-exam-table");
 const ExamNumber = document.getElementById("exam-number");
 
+const ExamDetails = e => {
+  let id = e.target.parentElement.id;
+  AllExamsTable.style.display = "none";
+  SingleExamTable.style.display = "flex";
+  AppendStudents(id);
+};
+
+AllExamsTable.addEventListener("click", ExamDetails);
+
 const FetchData = async url => {
   try {
     const response = await fetch(url);
@@ -28,10 +37,8 @@ const ExamRowCreator = (exam, counter) => {
 const AppendExams = () => {
   FetchData("/api/v1/exams")
     .then(exams => {
-      let counter = 1;
-      exams.exams.forEach(exam => {
-        ExamRowCreator(exam, counter);
-        counter++;
+      exams.exams.forEach((exam, i) => {
+        ExamRowCreator(exam, i + 1);
       });
     })
     .catch(error => {
@@ -40,3 +47,42 @@ const AppendExams = () => {
 };
 
 AppendExams();
+
+// Exam Student Logic
+
+const StudentRowCreator = (student, counter, studentRanking) => {
+  let row = SingleExamTable.insertRow(-1);
+  row.setAttribute("id", `${student.id}`);
+  let cell1 = row.insertCell(0);
+  let cell2 = row.insertCell(1);
+  let cell3 = row.insertCell(2);
+  cell1.innerHTML = `Student ${counter}`;
+  let studentGrade = Math.round(student.score * 100);
+  cell2.innerHTML = `${studentGrade}%`;
+  cell3.innerHTML = `${studentRanking[student.studentId]}`;
+};
+
+const rankStudents = students => {
+  let studentRanking = {};
+  let rankedStudents = [...students].sort((a, b) => a.score - b.score);
+  rankedStudents.forEach((student, i) => {
+    studentRanking[student.studentId] = i + 1;
+  });
+
+  return studentRanking;
+};
+
+const AppendStudents = examId => {
+  FetchData(`/api/v1/exams/${examId}`)
+    .then(students => {
+      let studentRanking = rankStudents(students.results);
+      
+      students.results.forEach((student, i) => {
+        StudentRowCreator(student, i + 1, studentRanking);
+      });
+
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
